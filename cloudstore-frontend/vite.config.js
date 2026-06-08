33294bc3b6f6cd9208b33afb955ea5f06b61b99a
Linkan <thelinussandelin@gmail.com>
2026-06-08 09:29:43 +0200

    Restore frontend package files

--- cloudstore-frontend/vite.config.js
+++ cloudstore-frontend/vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        allowedHosts: ['thecloudstore-product.duckdns.org'],
-        host: true
-    }
+  plugins: [react()],
 })
