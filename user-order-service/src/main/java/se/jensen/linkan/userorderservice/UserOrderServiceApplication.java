b6859df64b8fd8d60aae335b78366584d09de27c
Linkan <thelinussandelin@gmail.com>
2026-06-08 09:10:43 +0200

    Test push

--- user-order-service/src/main/java/se/jensen/linkan/userorderservice/UserOrderServiceApplication.java
+++ user-order-service/src/main/java/se/jensen/linkan/userorderservice/UserOrderServiceApplication.java
@@ -3,6 +3,7 @@ package se.jensen.linkan.userorderservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// @EnableDiscovery
 @SpringBootApplication
 public class UserOrderServiceApplication {
 
